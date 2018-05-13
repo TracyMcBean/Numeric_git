705f30b8dfe09798c2a09fdb74cd0284be65b063
TracyMcBean <thkisz@yahoo.co.uk>
2018-05-13 13:45:31 +0200

    Uebung 4 Aufgabe 1

--- run_1_4.m
+++ run_1_4.m
@@ -5,20 +5,21 @@
 function result = run_1_4()
     %RUN_1_4() loest lineares Ausgleichsproblem fuer verschieden Gitter
     
-    disp('Test 1, Gitter mit m = 2 und n = 1:')
-    result = givens_rotation(2,1);
-    fprintf('Test result: %s \n', result)
-    
-    disp('----------------------------------------------')
-    disp('Test 2, Gitter mit m = 4 und n = 2:')
-    result =  givens_rotation(4,2);
-    fprintf('Test result:\n')
+    disp('Test 1, Gitter mit m =3 und Polynom Grad n = 1:')
+    result = givens_rotation(3,1);
+    fprintf('Ergebnis fuer u:\n')
     disp(result)
     
     disp('----------------------------------------------')
-    disp('Test 3, Gitter mit m = 10 und n = 3:')
+    disp('Test 2, Gitter mit m = 10 und Polynom Grad n = 3:')
     result =  givens_rotation(10,3);
-    fprintf('Test result:\n')
+    fprintf('Ergebnis fuer u:\n')
+    disp(result)
+    
+    disp('----------------------------------------------')
+    disp('Test 3, Gitter mit m = 4 und Polynom Grad n = 4:')
+    result =  givens_rotation(4,4);
+    fprintf('Ergebnis fuer u:\n')
     disp(result)
     
 end %function
@@ -29,13 +30,23 @@ function result = givens_rotation(m, n)
     % n: Grad vom Polynom
     % m: Gitteraufloesung
     
+    % Fange Fehler ab wenn m zu niedrig ist
+    if (m < 3)
+      disp('Error: m must be bigger than 2!')
+      exit;
+    elseif (m < n)
+      disp('Error: m must be bigger than n!')
+      exit;
+    end
+    
     % Initialisiere
     x = zeros(m, 1);
     b = zeros(m, 1);
     xcooef = zeros(m,1);  % fuer matrix A gebraucht
-    A = zeros(m,n+1);
+    A = zeros(m,n);
     x(m)= 2*pi;
     xcooef(:) = 1;
+    n = n+1;    % Weil polynom vom Grad n zusaetzlich x^0 beinhaltet
     
     % Falls mehr als 2 Gitterpunkte setze die Abstaende entsprechend.
     if ( m > 2)
@@ -50,50 +61,65 @@ function result = givens_rotation(m, n)
     end 
     
     % Matrix A auffuellen mit t
-    for i = 1:(n+1)
+    for i = 1:(n)
         A(:, i) = xcooef;
         xcooef = xcooef.* x;
     end 
     
-    % Erstelle 3D-Matrix mit Einheitsmatrix
+    % Erstelle 3D-Matrix mit Einheitsmatrix die spaeter aufgefuellt wird.
     Q_single = eye(m);
-    % TO DO number of Qs
-    for k = 1:(m*n)
+    for k = 1:(m*n-m)
         Q(:,:,k) = Q_single;
     end 
     
     % Berechne die Transformationsmatrix
     count = 1;
-    for i = 1:(n+1)
+    for i = 1:(n)
         a = A(:,i);
-        l = 1;
-        k = 1;
+        l = i;
+        k = l+1;
         while (l< m)
             a_l = a(l);
-            a_k = a(l+1);
+            a_k = a(k);
             r = sqrt(a_l^2 + a_k^2);
             s = a_k / r;
             c = a_l / r;        
             
-            Q(l,k,count) = -s;
+            Q(l,k,count) = s;
             Q(l,l,count) = c;
-            Q(k,l,count) = s;
+            Q(k,l,count) = -s;
             Q(k,k,count) = c;
             
             count = count +1;
             l = l + 1;
+            k = k + 1;
         end   
     end
-  
-    Qnew = Q(:,:,1);
-    for i = 2:(count-1)
+    
+    % Multipliziere Transformationsmatrizen miteinander.
+    Qnew = Q(:,:, count-1 );
+    for i = count-1 : -1 : 1
         Qnew = Qnew * Q(:,:,i);
     end
+
     QT = transpose(Qnew);
     
+    % Berechne neuen b Vector
     bEnde = QT * b;
-    b_1 = bEnde(1:n+1);
+    b_1 = bEnde(1:n);
     
-    result = b_1;
+    
+    QTtimesA = QT * A;
+    
+    % Berechne R 
+    R = QTtimesA(1:n, 1:n);
+    
+    % Berechne x um damit u zu bestimmen
+    x = inv(R) * b_1;
+    fprintf('Berechnetes x: \n')
+    disp(x);
+    
+    u = A*x;
+    result = u;
     
 end %function
